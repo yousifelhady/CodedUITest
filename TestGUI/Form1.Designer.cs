b7d06fc720388fa4dd3c42abddebc95540a0b629
Yousif Gomaa <yousif.gomaa@valeo.com>
2018-02-12 15:51:24 +0200

    pushing test files and folders

---
+++ TestGUI/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace TestGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu21ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu31ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu32ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu41ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu1ToolStripMenuItem,
+            this.menu2ToolStripMenuItem,
+            this.menu3ToolStripMenuItem,
+            this.menu4ToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(870, 28);
+            this.menuStrip2.TabIndex = 0;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menu1ToolStripMenuItem
+            // 
+            this.menu1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subMenu11ToolStripMenuItem,
+            this.subMenu12ToolStripMenuItem});
+            this.menu1ToolStripMenuItem.Name = "menu1ToolStripMenuItem";
+            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.menu1ToolStripMenuItem.Text = "Menu1";
+            // 
+            // subMenu11ToolStripMenuItem
+            // 
+            this.subMenu11ToolStripMenuItem.Name = "subMenu11ToolStripMenuItem";
+            this.subMenu11ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu11ToolStripMenuItem.Text = "SubMenu1.1";
+            this.subMenu11ToolStripMenuItem.Click += new System.EventHandler(this.subMenu11ToolStripMenuItem_Click);
+            // 
+            // subMenu12ToolStripMenuItem
+            // 
+            this.subMenu12ToolStripMenuItem.Name = "subMenu12ToolStripMenuItem";
+            this.subMenu12ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu12ToolStripMenuItem.Text = "SubMenu1.2";
+            this.subMenu12ToolStripMenuItem.Click += new System.EventHandler(this.subMenu12ToolStripMenuItem_Click);
+            // 
+            // menu2ToolStripMenuItem
+            // 
+            this.menu2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subMenu21ToolStripMenuItem,
+            this.subMenu22ToolStripMenuItem});
+            this.menu2ToolStripMenuItem.Name = "menu2ToolStripMenuItem";
+            this.menu2ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.menu2ToolStripMenuItem.Text = "Menu2";
+            // 
+            // subMenu21ToolStripMenuItem
+            // 
+            this.subMenu21ToolStripMenuItem.Name = "subMenu21ToolStripMenuItem";
+            this.subMenu21ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu21ToolStripMenuItem.Text = "SubMenu2.1";
+            this.subMenu21ToolStripMenuItem.Click += new System.EventHandler(this.subMenu21ToolStripMenuItem_Click);
+            // 
+            // subMenu22ToolStripMenuItem
+            // 
+            this.subMenu22ToolStripMenuItem.Name = "subMenu22ToolStripMenuItem";
+            this.subMenu22ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu22ToolStripMenuItem.Text = "SubMenu2.2";
+            this.subMenu22ToolStripMenuItem.Click += new System.EventHandler(this.subMenu22ToolStripMenuItem_Click);
+            // 
+            // menu3ToolStripMenuItem
+            // 
+            this.menu3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subMenu31ToolStripMenuItem,
+            this.subMenu32ToolStripMenuItem});
+            this.menu3ToolStripMenuItem.Name = "menu3ToolStripMenuItem";
+            this.menu3ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.menu3ToolStripMenuItem.Text = "Menu3";
+            // 
+            // subMenu31ToolStripMenuItem
+            // 
+            this.subMenu31ToolStripMenuItem.Name = "subMenu31ToolStripMenuItem";
+            this.subMenu31ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu31ToolStripMenuItem.Text = "SubMenu3.1";
+            this.subMenu31ToolStripMenuItem.Click += new System.EventHandler(this.subMenu31ToolStripMenuItem_Click);
+            // 
+            // subMenu32ToolStripMenuItem
+            // 
+            this.subMenu32ToolStripMenuItem.Name = "subMenu32ToolStripMenuItem";
+            this.subMenu32ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu32ToolStripMenuItem.Text = "SubMenu3.2";
+            this.subMenu32ToolStripMenuItem.Click += new System.EventHandler(this.subMenu32ToolStripMenuItem_Click);
+            // 
+            // menu4ToolStripMenuItem
+            // 
+            this.menu4ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subMenu41ToolStripMenuItem});
+            this.menu4ToolStripMenuItem.Name = "menu4ToolStripMenuItem";
+            this.menu4ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.menu4ToolStripMenuItem.Text = "Menu4";
+            // 
+            // subMenu41ToolStripMenuItem
+            // 
+            this.subMenu41ToolStripMenuItem.Name = "subMenu41ToolStripMenuItem";
+            this.subMenu41ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.subMenu41ToolStripMenuItem.Text = "SubMenu4.1";
+            this.subMenu41ToolStripMenuItem.Click += new System.EventHandler(this.subMenu41ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(870, 608);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip2;
+            this.Name = "Form1";
+            this.Text = "WindowsAppTest";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem menu1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu11ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu12ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menu2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu21ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu22ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menu3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu31ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu32ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menu4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu41ToolStripMenuItem;
+    }
+}
+
---
+++ TestGUI/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void subMenu11ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu1.1", "Menu1.1");
+        }
+
+        private void subMenu12ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu1.2", "Menu1.2");
+        }
+
+        private void subMenu21ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu2.1", "Menu2.1");
+        }
+
+        private void subMenu22ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu2.2", "Menu2.2");
+        }
+
+        private void subMenu31ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu3.1", "Menu3.1");
+        }
+
+        private void subMenu32ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu3.2", "Menu3.2");
+        }
+
+        private void subMenu41ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello From SubMenu4.1", "Menu4.1");
+        }
+    }
+}
